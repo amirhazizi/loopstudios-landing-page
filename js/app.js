1dd5ad07a8b631b5df67b819a2dbec085483eb8b
amir <amirazizihosein@gmail.com>
2023-02-26 23:11:34 +0330

    add scrollreveal

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const menuBtn = document.querySelector("#hamburger-btn")
 const closeBtn = document.querySelector("#close-btn")
 const submenu = document.querySelector(".sidebar")
 const links = document.querySelectorAll(".link")
+const articles = document.querySelectorAll(".article")
+
+let delayTime = 100
+
 menuBtn.addEventListener("click", () => {
   submenu.classList.add("active")
   menuBtn.classList.add("diactive")
@@ -31,10 +35,28 @@ for (const link of links) {
   })
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  new TypeIt("#header-text", {
-    strings: "Immersive experiences that deliver",
-    speed: 50,
-    startDelay: 400,
-  }).go()
+new TypeIt("#header-text", {
+  strings: "Immersive experiences that deliver",
+  speed: 30,
+  startDelay: 150,
+}).go()
+
+articles.forEach((article, index) => {
+  delayTime += 100
+  const obj = {
+    delay: delayTime,
+    distance: "75px",
+  }
+  if (index % 2 === 0) {
+    ScrollReveal().reveal(article, {
+      ...obj,
+      origin: "left",
+    })
+  } else {
+    ScrollReveal().reveal(article, {
+      ...obj,
+      origin: "right",
+    })
+  }
 })
+document.querySelector("#footer-date").innerHTML = new Date().getFullYear()
