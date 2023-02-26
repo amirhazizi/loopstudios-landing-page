71305af4ef33502b25975571a12fc3a485a95b1b
amir <amirazizihosein@gmail.com>
2023-02-26 23:49:25 +0330

    add scroll-bar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
+// variables
 const menuBtn = document.querySelector("#hamburger-btn")
 const closeBtn = document.querySelector("#close-btn")
 const submenu = document.querySelector(".sidebar")
 const links = document.querySelectorAll(".link")
 const articles = document.querySelectorAll(".article")
 
+// scroll reveal var
 let delayTime = 100
 
+// submenu btn listeners
 menuBtn.addEventListener("click", () => {
   submenu.classList.add("active")
   menuBtn.classList.add("diactive")
@@ -19,28 +22,42 @@ closeBtn.addEventListener("click", () => {
   document.body.classList.remove("noscroll")
 })
 
+// scroll bar scroll event
+window.addEventListener("scroll", () => {
+  const post = window.scrollY
+  const bodyHeight = document.body.getBoundingClientRect().height
+  document.body.style.setProperty(
+    "--width",
+    `${Math.ceil((post / bodyHeight) * 100)}%`
+  )
+})
+
+// underline on active elements
 for (const link of links) {
   link.addEventListener("mouseenter", () => {
     const underline = document.createElement("div")
     underline.setAttribute("class", "underline")
     link.appendChild(underline)
     setTimeout(() => {
-      const under = link.querySelector(".underline")
-      under.classList.add("underline-actived")
-    }, 50)
+      link.querySelector(".underline").classList.add("underline-actived")
+    }, 8)
   })
   link.addEventListener("mouseleave", () => {
-    if (link.lastElementChild.classList.contains("underline"))
-      link.removeChild(link.lastElementChild)
+    if (link.lastElementChild.tagName.toLowerCase() === "div") {
+      if (link.lastElementChild.classList.contains("underline"))
+        link.removeChild(link.lastElementChild)
+    }
   })
 }
 
+// typeIt
 new TypeIt("#header-text", {
   strings: "Immersive experiences that deliver",
   speed: 30,
   startDelay: 150,
 }).go()
 
+// scroll reveal
 articles.forEach((article, index) => {
   delayTime += 100
   const obj = {
@@ -59,4 +76,6 @@ articles.forEach((article, index) => {
     })
   }
 })
+
+// set current year
 document.querySelector("#footer-date").innerHTML = new Date().getFullYear()
